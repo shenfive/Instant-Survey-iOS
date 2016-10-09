f98622a26913a4c484bac4408ed4802931e17768
申潤五 <shenrunwu@shenrunwude-macbook-pro.local>
2016-10-10 00:54:53 +0800

    Initial Commit

---
+++ instsuv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instsuv
+//
+//  Created by 申潤五 on 2016/10/10.
+//  Copyright © 2016年 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
